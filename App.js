226617a9275e17bf0eaf175f9b0344e6d38fac85
George Zalepa <george.zalepa@gmail.com>
2018-03-25 15:16:36 -0400

    :recycle: Move text input to separate component

--- App.js
+++ App.js
@@ -6,22 +6,20 @@ import {
   Text,
   TextInput,
 } from 'react-native';
+import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
   render() {
-    return (<KeyboardAvoidingView style={styles.container} behavior="padding">
-      <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-      <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-      <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
-      <TextInput
-        autoCorrect={false}
-        placeholder="Search any city"
-        placeholderTextColor="white"
-        style={styles.textInput}
-        clearButtonMode="always"
-      />
-    </KeyboardAvoidingView>);
+        <SearchInput placeholder="Search any city" />
+
+      </KeyboardAvoidingView>
+    );
   }
 }
 
@@ -48,15 +46,5 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18,
-  },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
   },
 });
