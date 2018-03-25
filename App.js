7b52be8d4f320aa9b9de885766e6bbe191081e4c
George Zalepa <george.zalepa@gmail.com>
2018-03-25 14:58:15 -0400

    Revert "Add static text fields to App"
    This reverts commit cc0e4708b3706b0fee14d43ed5481c340daf6ab4.

--- App.js
+++ App.js
@@ -1,45 +1,23 @@
 import React from 'react';
-import {
-  KeyboardAvoidingView,
-  Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
-    return (<View style={styles.container}>
-      <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-      <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-      <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-    </View>);
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
     flex: 1,
-    justifyContent: 'center'
-  },
-  textStyle: {
-    textAlign: 'center',
-    ...Platform.select({
-      ios: {
-        fontFamily: 'AvenirNext-Regular',
-      },
-      android: {
-        fontFamily: 'Roboto',
-      },
-    })
-  },
-  largeText: {
-    fontSize: 44,
-  },
-  smallText: {
-    fontSize: 18,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
